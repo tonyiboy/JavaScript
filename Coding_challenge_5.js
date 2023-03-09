e84d62facac3717642737fe4d4734364165c6c2c
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-09 16:59:20 +0100

    Coding challenge 5

---
+++ Coding_challenge_5.js
@@ -0,0 +1,13 @@
+//Write a program that launches a carousel for 10 turns, showing the turn number each time.
+
+for (let i = 1; i <=10; i++){
+    console.log(i)
+}
+
+//When it's done, improve it so that the number of turns is given by the user.
+
+let turns = prompt("How many turns should we do",0)
+
+for (let i = 1; i <=turns; i++){
+    console.log(i)
+}
