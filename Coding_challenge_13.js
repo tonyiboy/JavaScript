b7c1763e6eb7e0499dcbf0e64e4bc874c0fd504f
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-12 19:29:33 +0100

    Coding challenge 13

---
+++ Coding_challenge_13.js
@@ -0,0 +1,15 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+let tag = "#"
+while (tag.length <= 7){
+    console.log(tag)
+    tag += "#"
+}
