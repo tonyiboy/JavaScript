d79393d4afdbd87cd2de5a708da4518eabae28f7
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-12 20:54:07 +0100

    Coding challenge 14

---
+++ Coding_challenge_14.js
@@ -0,0 +1,33 @@
+// Chessboard
+// Write a program that creates a string that represents an 8×8 grid, using
+// newline characters to separate lines. At each position of the grid there is either
+// a space or a "#" character. The characters should form a chessboard.
+
+// Passing this string to console.log should show something like this:
+
+// # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # #
+
+// When you have a program that generates this pattern, define a binding size =
+// 8 and change the program so that it works for any size, outputting a grid of
+// the given width and height.
+
+let size = 10
+let board = ""
+for (let row = 1; row <= size; row++){
+    for (let column = 1; column <= size; column++){
+        if ((row+column)%2 == 0){
+            board += " "
+        } else {
+            board += "#"
+        }
+    }
+    board += "\n"
+}
+console.log(board)
