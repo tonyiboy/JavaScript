942fdff83645cf9d77f03ae3c43f1ad6eeb0949f
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-14 16:31:30 +0100

    Coding challenge 16

---
+++ Coding_challenge_16.js
@@ -0,0 +1,16 @@
+// The previous chapter introduced the standard function Math.min that returns its smallest argument. 
+// We can build something like that now. 
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(num1, num2){
+    if (num1 < num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
+
+console.log(min(0, 10));
+// // → 0
+console.log(min(0, -10));
+// // → -10
