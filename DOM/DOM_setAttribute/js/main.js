a4978ee335ff73507b05de3180443bcb59ea6209
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-02 01:26:37 +0100

    DOM_setAttribute

---
+++ DOM/DOM_setAttribute/js/main.js
@@ -0,0 +1,21 @@
+// append() and createElement()
+
+
+// create element
+
+const ul = document.querySelector("ul")
+const li = document.createElement("li")
+
+// adding element
+
+ul.append(li)
+
+// modifying the text
+
+li.innerText = "movie 6"
+
+// modifying attribute
+
+li.setAttribute("id", "title")
+
+
