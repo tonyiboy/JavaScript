a486d8cab402e298774d59b87b8c8406f9dfb87e
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-03 18:20:33 +0100

    DOM_classList_remove

---
+++ DOM/DOM_classList_remove/js/main.js
@@ -0,0 +1,20 @@
+// create element
+
+const ul = document.querySelector("ul")
+const li = document.createElement("li")
+
+// adding element
+
+ul.append(li)
+
+// modifying the text
+
+li.innerText = "movie 6"
+
+// modifying classes
+
+li.classList.add("list-items")
+
+li.classList.remove("list-items")
+
+
