87646a90f0022f6d755182738e88574e5a4fd1a3
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-04 03:54:02 +0100

    DOM_remove

---
+++ DOM/DOM_remove/js/main.js
@@ -0,0 +1,21 @@
+// create element
+
+const ul = document.querySelector("ul")
+const li = document.createElement("li")
+
+// adding element
+
+ul.append(li)
+
+// modifying the text
+
+li.innerText = "movie 6"
+
+// remove element
+
+li.remove()
+
+
+
+
+
