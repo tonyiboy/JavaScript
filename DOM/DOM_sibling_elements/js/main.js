c84cb0fc2038bc01fc182252c45be55916d8bd7e
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-05 01:27:11 +0100

    DOM_sibling_elements

---
+++ DOM/DOM_sibling_elements/js/main.js
@@ -0,0 +1,13 @@
+// sibling element
+
+let ul = document.querySelector("ul")
+
+const div = document.querySelector("div")
+
+// console.log(div.childNodes)
+
+console.log(ul.previousElementSibling)
+console.log(ul.nextElementSibling)
+
+
+
