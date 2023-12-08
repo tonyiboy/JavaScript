909bc77989b4271c7e479c9e6f935da7df6d5556
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-08 06:28:54 +0100

    practice_4.js

---
+++ JavaScript_practice/practice_4.js
@@ -0,0 +1,21 @@
+let day = "monday"
+
+switch(day) {
+  case "monday":
+    console.log("7am");
+    break;
+  case "tuesday":
+    console.log("8am");
+    break;
+  case "wednesday":
+    console.log("9am");
+    break;
+  case "thursday":
+    console.log("6am");
+    break;
+  case "friday":
+    console.log("7am");
+    break;
+  default:
+    console.log("rest day");
+}
