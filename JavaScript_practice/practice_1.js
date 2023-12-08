2f5c611d55be7fb4c2afd78784963a4bf4b11f95
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-08 05:27:14 +0100

    practice_1.js

---
+++ JavaScript_practice/practice_1.js
@@ -0,0 +1,11 @@
+let num1 = 2
+let num2 = 3
+let num3 = 4
+
+if (num1 > num2 && num1 > num3) {
+    console.log("num1 is greatest")
+} else if (num2 > num1 && num2 > num3) {
+    console.log("num2 is greatest")
+} else {
+    console.log("num3 is greatest")
+}
