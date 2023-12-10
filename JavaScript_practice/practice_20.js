c22508fe7dbf6144dd42fdbf692f6d66c7b53bf8
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-10 06:13:58 +0100

    practice_20.js

---
+++ JavaScript_practice/practice_20.js
@@ -0,0 +1,5 @@
+//Create and array of tv shows. Loop through and print each show to the console
+
+let tvShows = ["zombie","walking dead","hercules","scream"]
+
+tvShows.forEach(show => console.log(show))
