cc5b4bd951ce865c5a5a9b7715fd807ff8c1afa7
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-10 04:47:46 +0100

    practice_14.js

---
+++ JavaScript_practice/practice_14.js
@@ -0,0 +1,6 @@
+// *Variables*
+// Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
+
+let favHolidays = "Halloween"
+favHolidays = favHolidays.toUpperCase()
+console.log(favHolidays);
