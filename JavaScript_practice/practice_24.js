281ef18dcfcb6fff734a9e6320df31ba016a66c9
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-13 15:59:53 +0100

    practice_24.js

---
+++ JavaScript_practice/practice_24.js
@@ -0,0 +1,7 @@
+// Iterate over the fruits array to log I want to eat a plus the name of each fruit to the console. 
+// For example, I want to eat a mango.
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(myFavoriteFruits => console.log(`I want to eat a ${myFavoriteFruits}.`))
