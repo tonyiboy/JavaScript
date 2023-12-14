364d85835e182689792ce296a3b38425e203380a
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-14 12:36:16 +0100

    practice_30.js

---
+++ JavaScript_practice/practice_30.js
@@ -0,0 +1,14 @@
+// let spaceship = {
+//     'Fuel Type': 'diesel',
+//     color: 'silver'
+//   };
+
+//   The spaceship we have so far looks good but, unfortunately, is not very fast at hyperspace travel 
+//   due to having an inferior fuel source. 
+//   Make a new spaceship object named fasterShip with the same color as spaceship but with a Fuel Type equal to 'Turbo Fuel'.
+
+// Write your fasterShip object literal below
+let fasterShip = {
+    "Fuel Type": "Turbo Fuel",
+    color: "silver"
+  }
