bb1614f7de2a914bc08e5a7698f960f7d82f1318
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-15 16:05:42 +0100

    practice_45.js

---
+++ JavaScript_practice/practice_45.js
@@ -0,0 +1,25 @@
+// Use destructured assignment to create a const variable named functionality that extracts the functionality property of robot.
+
+// If you need a reminder on how to use destructured assignment, review the example in the narrative or check the hint.
+
+// Since functionality is referencing robot.functionality we can call the methods available to robot.functionality 
+// simply through functionality.
+
+// Take advantage of this shortcut and call the .beep() method on functionality.
+
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+  };
+  
+  const {functionality} = robot
+  
+  functionality.beep()
