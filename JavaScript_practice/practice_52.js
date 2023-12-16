cc8c9f95cafd2e41d628971f0d5575874da3d1d8
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-16 14:38:15 +0100

    practice_52.js

---
+++ JavaScript_practice/practice_52.js
@@ -0,0 +1,17 @@
+// We have defined a function called countdown with one parameter (n). 
+// The function should use recursion to return an array containing the integers n through 1 based on the n parameter. 
+// If the function is called with a number less than 1, the function should return an empty array. For example, 
+// calling this function with n = 5 should return the array [5, 4, 3, 2, 1]. 
+// Your function must use recursion by calling itself and must not use loops of any kind.
+
+// Only change code below this line
+function countdown(n){
+    if (n < 1) {
+      return [];
+    } else {
+      const arr = countdown(n - 1);
+      arr.unshift(n);
+      return arr;
+    }
+  }
+  // Only change code above this line
