69be759ecde5fa5aacc47d9f317123aab38a3570
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-16 14:28:09 +0100

    practice_50.js

---
+++ JavaScript_practice/practice_50.js
@@ -0,0 +1,8 @@
+// Use the conditional operator in the checkEqual function to check if two numbers are equal or not. 
+// The function should return either the string Equal or the string Not Equal.
+
+function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal"
+   }
+   
+   checkEqual(1, 2);
