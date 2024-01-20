cd2e0a329af36c2f02909bde051b7502e7064ec9
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-20 12:41:09 +0100

    DOM_appendChild.js

---
+++ DOM/DOM_appendChild/js/main.js
@@ -0,0 +1,8 @@
+//we passing a string that is the tag name of the element that we want to create
+
+let olElement = document.createElement("ol") 
+console.log(olElement);
+
+// then we put it in html
+
+document.body.appendChild(olElement)
