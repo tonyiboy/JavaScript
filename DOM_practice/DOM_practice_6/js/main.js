fb3b6af00d7ba27865207cc1cba33ab9421be2a7
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-22 19:32:41 +0100

    DOM_practice_6.js

---
+++ DOM_practice/DOM_practice_6/js/main.js
@@ -0,0 +1,17 @@
+let myList = ["list number 1","list numebr 2", "list number 3"]
+
+let h1Element = document.getElementById("main")
+let olElement = document.createElement("ol")
+
+h1Element.insertAdjacentElement("afterend",olElement)
+
+// lets loop my array that will create list on my page
+
+myList.forEach(text => {
+    let liList = document.createElement("li")
+    liList.textContent = text
+
+    olElement.appendChild(liList)
+})
+
+olElement.removeChild(olElement.lastChild)
