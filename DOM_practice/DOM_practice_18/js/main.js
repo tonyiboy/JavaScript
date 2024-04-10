05c2c678b6e4ec0f01fd9c2128f2a4a88987610d
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-10 22:06:03 +0200

    DOM_practice_18

---
+++ DOM_practice/DOM_practice_18/js/main.js
@@ -0,0 +1,12 @@
+let liFirstTest = document.getElementById("list-one")
+
+let liSecondTest = document.getElementsByTagName("li")
+
+let list = document.getElementsByClassName("list")
+
+
+console.log(list);
+
+console.log(liSecondTest);
+
+console.log(liFirstTest);
