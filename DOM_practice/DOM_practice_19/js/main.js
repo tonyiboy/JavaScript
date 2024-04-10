70a20ffe75b7aef061043d87f14d58fc617dd8ae
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-10 22:15:25 +0200

    DOM_practice_19

---
+++ DOM_practice/DOM_practice_19/js/main.js
@@ -0,0 +1,12 @@
+let liFirstTest = document.querySelector(".list")
+
+let liSecondTest = document.querySelector("li")
+
+let list = document.querySelector("#list-one")
+
+
+console.log(list);
+
+console.log(liSecondTest);
+
+console.log(liFirstTest);
