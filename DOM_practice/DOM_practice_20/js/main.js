e6bc588b889d4dd09a013fa0a666dc70b7adf88e
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-10 22:20:54 +0200

    DOM_practice_20

---
+++ DOM_practice/DOM_practice_20/js/main.js
@@ -0,0 +1,12 @@
+let firstTest = document.querySelectorAll(".list")
+
+let secondTest = document.querySelectorAll("ul")
+
+let thirdTest = document.querySelectorAll("#list-one")
+
+
+console.log(firstTest);
+
+console.log(secondTest);
+
+console.log(thirdTest);
