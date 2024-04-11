4e32ddd2e00564f64584d25988d2e9811a95b0ee
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-11 22:57:38 +0200

    DOM_practice_25

---
+++ DOM_practice/DOM_practice_25/js/main.js
@@ -0,0 +1,6 @@
+let listTwo = document.querySelector(".list-two")
+let listThree = document.querySelector(".list-three")
+let ul = document.querySelector("ul")
+
+ul.replaceChild(listThree, listTwo)
+
