9476a18cd686ea7420c4e44655a02a870f829773
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-12 23:24:20 +0200

    DOM_practice_31

---
+++ DOM_practice/DOM_practice_31/js/main.js
@@ -0,0 +1,8 @@
+let createElement = document.createElement("p")
+let section = document.querySelector("section")
+createElement.innerText = "beforebegin text"
+
+section.insertAdjacentElement("beforeend" , createElement)
+
+
+
