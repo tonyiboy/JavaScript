0122ac22073830f53eaf3625c6f10bac0449b15f
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-19 22:28:19 +0200

    DOM_practice_59

---
+++ DOM_practice/DOM_practice_59/js/main.js
@@ -0,0 +1,31 @@
+let box = document.querySelector("div")
+let button = document.querySelector("button")
+
+let horizontalMovement = 50
+let verticalMovement = 50
+
+function clickMe() {
+
+    if(horizontalMovement < 800){
+    box.style.marginLeft = `${horizontalMovement}px`
+    horizontalMovement += 50
+    }else {
+        box.style.marginTop = `${verticalMovement}px`
+        verticalMovement += 50
+    }
+
+    if(horizontalMovement === 800){
+        box.style.backgroundColor = "purple"
+    }
+}
+
+button.addEventListener("click", clickMe )
+
+
+
+
+
+
+
+
+
