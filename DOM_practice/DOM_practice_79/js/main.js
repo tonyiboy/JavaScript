8c37ef1f2ca5856784a00ec9c799e8c210877bdb
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-25 23:26:45 +0200

    DOM_practice_79

---
+++ DOM_practice/DOM_practice_79/js/main.js
@@ -0,0 +1,10 @@
+let myList = document.querySelector(".list-two")
+
+console.log(myList.previousSibling);
+console.log(myList.nextSibling);
+
+console.log(myList.previousElementSibling);
+console.log(myList.nextElementSibling);
+
+
+
