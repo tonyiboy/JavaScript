db893eca041e1244ea404698adf71832fc3a83c4
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-25 23:11:05 +0200

    DOM_practice_78

---
+++ DOM_practice/DOM_practice_78/js/main.js
@@ -0,0 +1,9 @@
+let myList = document.querySelector("ul")
+
+console.log(myList.children);
+console.log(myList.firstElementChild);
+console.log(myList.lastElementChild);
+console.log(myList.children[2]);
+
+
+
