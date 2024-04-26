d0750ad6e27567e7fbbe343b5b601c82194c6a32
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-26 23:17:28 +0200

    DOM_practice_82

---
+++ DOM_practice/DOM_practice_82/js/main.js
@@ -0,0 +1,13 @@
+let list = document.querySelector("ul")
+
+console.log(list.childNodes);
+console.log(list.firstChild);
+console.log(list.lastChild);
+
+console.log(list.children);
+console.log(list.firstElementChild);
+console.log(list.lastElementChild);
+
+
+
+
