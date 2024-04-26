e540a0307e098240d04e32b52e481ac0792bcb52
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-26 23:30:03 +0200

    DOM_practice_85

---
+++ DOM_practice/DOM_practice_85/js/main.js
@@ -0,0 +1,16 @@
+let myID = document.getElementById("last")
+let myClass = document.getElementsByClassName("middle")
+let myTag = document.getElementsByTagName("ul")
+let myQuery = document.querySelector("ul")
+let myQueryAll = document.querySelectorAll("li")
+
+console.log(myID);
+console.log(myClass);
+console.log(myTag);
+console.log(myQuery);
+console.log(myQueryAll);
+
+
+
+
+
