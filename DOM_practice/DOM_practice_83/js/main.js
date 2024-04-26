1d3ea70c753b0e66bc231d8d0038363efe76de6b
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-26 23:20:39 +0200

    DOM_practice_83

---
+++ DOM_practice/DOM_practice_83/js/main.js
@@ -0,0 +1,11 @@
+let middleList = document.querySelector(".middle")
+
+console.log(middleList.previousSibling);
+console.log(middleList.nextSibling);
+
+console.log(middleList.previousElementSibling);
+console.log(middleList.nextElementSibling);
+
+
+
+
