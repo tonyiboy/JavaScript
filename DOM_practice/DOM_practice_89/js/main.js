0a50b04959713bd026ee7aa92a9440cd36bed68f
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-27 23:18:55 +0200

    DOM_practice_89

---
+++ DOM_practice/DOM_practice_89/js/main.js
@@ -0,0 +1,17 @@
+let list = document.querySelector("ul")
+let middle = document.querySelector(".middle")
+let createElement = document.createElement("li")
+createElement.innerText= "this is insert before list"
+
+list.insertBefore(createElement, middle)
+
+
+
+
+
+
+
+
+
+
+
