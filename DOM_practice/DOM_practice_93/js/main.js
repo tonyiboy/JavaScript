b219977d149b06d6b7f00777a8a6acd9ef0db9b9
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-28 23:40:05 +0200

    DOM_practice_93

---
+++ DOM_practice/DOM_practice_93/js/main.js
@@ -0,0 +1,18 @@
+let ulList = document.querySelector("ul")
+
+console.log(ulList.childNodes);
+console.log(ulList.firstElementChild);
+console.log(ulList.lastElementChild);
+
+
+
+
+
+
+
+
+
+
+
+
+
