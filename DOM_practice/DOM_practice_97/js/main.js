6aabade3060cf8e958554f90274f2a5f1bf3b59d
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-29 20:53:15 +0200

    DOM_practice_97

---
+++ DOM_practice/DOM_practice_97/js/main.js
@@ -0,0 +1,33 @@
+let list = document.querySelector("ul")
+let li = document.querySelector(".middle")
+
+console.log(li.parentNode);
+console.log(li.parentElement);
+
+console.log(list.childNodes);
+console.log(list.firstChild);
+console.log(list.lastChild);
+
+console.log(list.children);
+console.log(list.firstElementChild);
+console.log(list.lastElementChild);
+
+console.log(li.previousSibling);
+console.log(li.nextSibling);
+
+console.log(li.previousElementSibling);
+console.log(li.nextElementSibling);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
