3cee26acd3f9a0d754275e364b338ce424ea9305
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-01 23:13:11 +0200

    DOM_practice_108

---
+++ DOM_practice/DOM_practice_108/js/main.js
@@ -0,0 +1,41 @@
+let id = document.getElementById("first")
+console.log(id);
+
+let className = document.getElementsByClassName("middle")
+console.log(className);
+
+let tagName = document.getElementsByTagName("li")
+console.log(tagName);
+
+let query = document.querySelector("ul")
+console.log(query);
+
+let queryAll = document.querySelectorAll("li")
+console.log(queryAll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
