1f1526de063df3a95a58cd52504b32f9000ffd7b
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-03 23:09:20 +0200

    DOM_practice_117

---
+++ DOM_practice/DOM_practice_117/js/main.js
@@ -0,0 +1,45 @@
+// text color button
+let textColor = document.querySelector(".text-color")
+let p = document.querySelector("p")
+
+textColor.addEventListener("click", () => {
+    p.style.color = "#FF4081"
+})
+
+// background color
+let backgroundColor = document.querySelector(".background-color")
+let main = document.querySelector("main")
+
+backgroundColor.addEventListener("click", () => {
+    main.style.backgroundColor = "#2E8B57"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
