92a7a6875e937d2f924388fe77ad81ddcdac38c0
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-11 23:39:30 +0200

    DOM_practice_140

---
+++ DOM_practice/DOM_practice_140/js/main.js
@@ -0,0 +1,24 @@
+let button = document.querySelector("button")
+let ol = document.querySelector("ol")
+
+button.addEventListener("click", () => {
+    if(ol.hasAttribute("reversed")) {
+        ol.removeAttribute("reversed")
+    }else ol.setAttribute("reversed", "true")
+})
+
+// color
+let buttonColor = document.querySelector(".btn-color")
+
+buttonColor.addEventListener("click", () => {
+    ol.style.color = "red"
+    })
+    
+
+console.log(ol.children);
+console.log(ol.firstChild);
+console.log(ol.lastChild);
+
+console.log(ol.children);
+console.log(ol.firstElementChild);
+console.log(ol.lastElementChild);
