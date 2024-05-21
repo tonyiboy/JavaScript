eab3078b35451e33fb673602f7e5721cbf6ad2fc
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-21 23:28:24 +0200

    DOM_practice_172

---
+++ DOM_practice/DOM_practice_172/js/main.js
@@ -0,0 +1,32 @@
+let buttonRight = document.querySelector(".right")
+let buttonDown = document.querySelector(".down")
+
+let buttonLeft = document.querySelector(".left")
+let buttonUp = document.querySelector(".up")
+
+let p = document.querySelector("p")
+
+buttonRight.addEventListener("click", () => {
+    p.style.transform += "translateX(10px)"
+})
+
+buttonDown.addEventListener("click", () => {
+    p.style.transform += "translateY(10px)"
+})
+
+buttonLeft.addEventListener("click", () => {
+    p.style.transform += "translateX(-10px)"
+})
+
+buttonUp.addEventListener("click", () => {
+    p.style.transform += "translateY(-10px)"
+
+})
+
+
+console.log(buttonDown.previousSibling);
+console.log(buttonDown.nextSibling);
+
+console.log(buttonDown.previousElementSibling);
+console.log(buttonDown.nextElementSibling);
+
