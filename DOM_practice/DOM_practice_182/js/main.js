971c15bfb92688e11de5eff718f61429b972eaca
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-24 23:03:18 +0200

    DOM_practice_182

---
+++ DOM_practice/DOM_practice_182/js/main.js
@@ -0,0 +1,24 @@
+let buttonLeft = document.querySelector(".left")
+let buttonRight = document.querySelector(".right")
+let buttonUp = document.querySelector(".up")
+let buttonDown = document.querySelector(".down")
+
+let section = document.querySelector("section")
+
+buttonLeft.addEventListener("click", () => {
+    section.style.transform += "translateX(-10px)"
+})
+
+buttonRight.addEventListener("click", () => {
+    section.style.transform += "translateX(+10px)"
+})
+
+buttonUp.addEventListener("click", () => {
+    section.style.transform += "translateY(-10px)"
+})
+
+buttonDown.addEventListener("click", () => {
+    section.style.transform += "translateY(+10px)"
+})
+
+
