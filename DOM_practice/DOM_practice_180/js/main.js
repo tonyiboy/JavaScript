f9e35cb10181aa4d23b311a270c839f8b9ba3686
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-24 22:54:53 +0200

    DOM_practice_180

---
+++ DOM_practice/DOM_practice_180/js/main.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("button")
+let section = document.querySelector("section")
+
+button.addEventListener("click", () => {
+    section.style.transform += "translate(-10px)"
+})
