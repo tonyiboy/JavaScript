7070ab52a2cef1a7f64ac33c91c75d1b829770b6
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-29 23:06:09 +0200

    Canvas_practice_4

---
+++ Canvas_practice/Canvas_practice_4/js/main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "red"
+ctx.fillRect(10, 30, 100, 100)
+
+ctx.fillStyle = "green"
+ctx.fillRect(130,30, 100, 100)
+
+ctx.fillStyle = "white"
+ctx.fillRect(250, 30, 100, 100)
+
+ctx.fillStyle = "black"
+ctx.fillRect(370, 30, 100, 100)
+
+ctx.fillStyle = "yellow"
+ctx.fillRect(490, 30, 100, 100)
+
+
