90faafcde97a5707667c23d208c5ecd79eb16135
tonyiboy <antoniocodinggithub@gmail.com>
2024-05-30 13:51:54 +0200

    Canvas_practice_7

---
+++ Canvas_practice/Canvas_practice_7/js/main.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "red"
+ctx.fillRect(30, 30, 100, 100)
+
+ctx.lineWidth = 5
+ctx.strokeStyle = "red"
+ctx.strokeRect(450, 30, 100, 100)
+
+
+
