0647823a8b1690b8569590c2043302474ae29f9c
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-03 22:48:42 +0200

    DOM_practice_201

---
+++ DOM_practice/DOM_practice_201/js/main.js
@@ -0,0 +1,20 @@
+let myLink = document.querySelector("a").href
+let myLinkTwo = document.querySelector("a").getAttribute("href")
+console.log(myLink);
+console.log(myLinkTwo);
+
+let list = document.querySelector("ul").getAttribute("id")
+let listTwo = document.querySelector("ul").id
+console.log(list);
+console.log(listTwo);
+
+
+
+
+
+
+
+
+
+
+
