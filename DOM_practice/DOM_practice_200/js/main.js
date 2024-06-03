123a5191e0d9354528999da1bd4ef183c0df6aa5
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-03 22:28:39 +0200

    DOM_practice_200

---
+++ DOM_practice/DOM_practice_200/js/main.js
@@ -0,0 +1,17 @@
+// const elements = Array.from(document.getElementsByClassName("para"))
+
+// elements.forEach(element => {
+//     console.log(element);
+// });
+
+const elements = document.getElementsByClassName("para")
+
+for(let i = 0; i < elements.length; i++) {
+    console.log(elements[i]);
+}
+
+
+
+
+
+
