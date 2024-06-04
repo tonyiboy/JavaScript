091afed85c5a551a1df95b65c79f02dac194d3fd
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-04 15:04:33 +0200

    DOM_practice_205

---
+++ DOM_practice/DOM_practice_205/js/main.js
@@ -0,0 +1,27 @@
+function linkInfo(element) {
+    let links = document.getElementsByTagName("a")
+    let attributeValue = document.querySelector("a").getAttribute("href")
+    console.log(links.length);
+    console.log(attributeValue);
+   
+
+    
+
+}
+
+linkInfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
