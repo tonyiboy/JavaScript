a8b60b8b001997d1fd8196e5ef5efbc1864fdaed
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-04 15:09:37 +0200

    DOM_practice_207

---
+++ DOM_practice/DOM_practice_207/js/main.js
@@ -0,0 +1,23 @@
+let button = document.querySelector("button")
+let h1 = document.querySelector("h1")
+
+button.addEventListener("click", () => {
+    h1.remove()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
