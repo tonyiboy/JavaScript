a1d72945be4fc6a5d7a3bba129a0f2f11aa779e1
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-04 15:07:24 +0200

    DOM_practice_206

---
+++ DOM_practice/DOM_practice_206/js/main.js
@@ -0,0 +1,21 @@
+let button = document.querySelector("button")
+let list = document.querySelector("ul")
+
+button.addEventListener("click", () => {
+    list.remove()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
