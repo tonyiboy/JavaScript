c9ec860908121809f83f8485eaecb814c5074f20
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-07 23:40:30 +0200

    DOM_practice_210

---
+++ DOM_practice/DOM_practice_210/js/main.js
@@ -0,0 +1,25 @@
+let p = document.querySelector("p")
+let button = document.querySelector("button")
+let body = document.querySelector("body")
+
+
+
+button.addEventListener("click", () => {
+    let cloned = p.cloneNode()
+    cloned.innerText = "good job"
+    let result = document.querySelector("#one").value
+    if(result === "hello") {
+        p.innerText = "hello"
+       
+    }else {
+        cloned.innerText ="bad job"
+    }
+    body.append(cloned)
+
+})
+
+console.log(body.firstChild);
+console.log(body.lastChild);
+
+console.log(body.firstElementChild);
+console.log(body.lastElementChild);
