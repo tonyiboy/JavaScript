1a3a596abbd884d834ddc5a66f1c2cfb930d066f
tonyiboy <antoniocodinggithub@gmail.com>
2024-06-12 23:48:09 +0200

    Canvas_practice_23

---
+++ Canvas_practice/Canvas_practice_23/js/main.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 500
+canvas.height = 500
+
+function draw(x,h,width,height) {
+  ctx.beginPath()
+  ctx.moveTo(x,h)
+  ctx.lineTo(width,height)  
+  ctx.stroke()
+}
+
+draw(30,30,130,50)
+
+
+
+
+
+
