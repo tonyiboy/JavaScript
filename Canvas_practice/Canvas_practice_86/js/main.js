6c8a753871a30e77d750af44b7ade05ee788d60a
tonyiboy <antoniocodinggithub@gmail.com>
2024-07-17 20:23:28 +0200

    Canvas_practice_86

---
+++ Canvas_practice/Canvas_practice_86/js/main.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+const circle = {
+    x:200,
+    y:200,
+    size:30,
+    dx:5,
+    dy:4
+}
+
+function drawCircle () {
+    ctx.beginPath()
+    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2)
+    ctx.fillStyle = "blue"
+    ctx.fill()
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    drawCircle()
+
+    circle.x += circle.dx
+    circle.y += circle.dy
+
+    requestAnimationFrame(update)
+}
+update()
