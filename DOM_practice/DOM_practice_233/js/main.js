0e9b3910d8dd5e4511e883093cc4d679aa91eaed
tonyiboy <antoniocodinggithub@gmail.com>
2024-07-22 23:44:58 +0200

    DOM_practice_233

---
+++ DOM_practice/DOM_practice_233/js/main.js
@@ -0,0 +1,35 @@
+let textColor = document.querySelector(".text-color")
+let p = document.querySelector("p")
+
+textColor.addEventListener("click", () => {
+    p.style.color = "#c6ffe6"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
