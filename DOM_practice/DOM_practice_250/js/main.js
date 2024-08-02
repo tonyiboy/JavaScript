5ce96ff663d2804de8a7a0f4962b876a1c5bc612
tonyiboy <antoniocodinggithub@gmail.com>
2024-08-02 23:14:14 +0200

    DOM_practice_250

---
+++ DOM_practice/DOM_practice_250/js/main.js
@@ -0,0 +1,12 @@
+let arr = ["movie1", "movie2","movie3", "movie4", "movie5","movie6","movie7","movie7","movie8","movie9","movie10"]
+console.log(arr)
+//converting my arr to a string
+let movies= arr.join(",")
+console.log(movies)
+
+//finding paragraph with id
+let p = document.getElementById("para")
+console.log(p)
+
+//put my movies inside paragraph
+p.textContent= movies
