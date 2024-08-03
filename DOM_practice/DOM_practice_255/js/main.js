69b26488a3bb2a50992aed6953eab188addb2277
tonyiboy <antoniocodinggithub@gmail.com>
2024-08-03 23:34:25 +0200

    DOM_practice_255

---
+++ DOM_practice/DOM_practice_255/js/main.js
@@ -0,0 +1,35 @@
+let body = document.querySelector("body")
+
+let pOne = document.createElement("p")
+let pTwo = document.createElement("p")
+let pThree = document.createElement("p")
+
+pOne.innerText = "this is p one"
+pTwo.innerText = "this is p two"
+pThree.innerText = "this is p three"
+
+body.append(pOne,pTwo,pThree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
