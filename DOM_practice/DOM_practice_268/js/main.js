8908a68d6ba51d4fd121e5d69d6c1bcab49d56ee
tonyiboy <antoniocodinggithub@gmail.com>
2024-08-06 23:10:16 +0200

    DOM_practice_268

---
+++ DOM_practice/DOM_practice_268/js/main.js
@@ -0,0 +1,28 @@
+let listId = document.getElementById("first-list")
+let listTag = document.getElementsByTagName("li")
+let listClass = document.getElementsByClassName("middle-list")
+
+let listQuery = document.querySelector("ul")
+let listQueryAll = document.querySelectorAll("li")
+
+console.log(listId);
+console.log(listTag);
+console.log(listClass);
+
+console.log(listQuery);
+console.log(listQueryAll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
