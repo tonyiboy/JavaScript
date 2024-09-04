f1fff0ae9ca916c6a23766cce20eb0dc0fc453b5
tonyiboy <antoniocodinggithub@gmail.com>
2024-09-04 23:44:59 +0200

    DOM_practice_291

---
+++ DOM_practice/DOM_practice_291/js/main.js
@@ -0,0 +1,18 @@
+let box = document.querySelector("div")
+let button = document.querySelector("button")
+
+function clickMe() {
+    box.style.margin = "auto"
+}
+
+
+button.addEventListener("click", clickMe )
+
+
+
+
+
+
+
+
+
