c2d43ae61069a57816cbccc172abd7c8a1c1adbb
tonyiboy <antoniocodinggithub@gmail.com>
2024-09-14 20:48:40 +0200

    DOM_practice_316

---
+++ DOM_practice/DOM_practice_316/js/main.js
@@ -0,0 +1,11 @@
+// even listener
+
+const buttonTwo = document.querySelector(".btn-2")
+
+
+buttonTwo.addEventListener("click", alretBtn)
+
+function alretBtn() {
+    alert("I love JavaScript")
+}
+
