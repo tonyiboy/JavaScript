ecadf4000e5bc95dbe040215bb054016de466845
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-18 17:36:11 +0200

    practice_72

---
+++ JavaScript_practice/practice_72.js
@@ -0,0 +1,28 @@
+// Define a method hello that returns "Hello, Name!" to a given name, 
+// or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a 
+// name with a first capital letter (Xxxx).
+
+// Examples:
+
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given 
+//   or `name` = ""        => return "Hello, World!"
+
+function hello(name) {
+
+    if (!name) return "Hello, World!"
+
+    let result = ""
+
+    for (let i = 0; i < name.length; i++) {
+        if(i === 0) {
+            result = name[0].toUpperCase()
+        }else result += name[i].toLowerCase()
+    }
+
+    return `Hello, ${result}!`
+
+}
