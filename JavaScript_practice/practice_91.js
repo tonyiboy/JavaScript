6479b8b590f9832f849bbc8f8df5eee12502e6fb
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-20 22:01:26 +0200

    practice_91

---
+++ JavaScript_practice/practice_91.js
@@ -0,0 +1,7 @@
+// Write function parseF which takes an input and returns a number or 
+// null if conversion is not possible. The input can be one of many different types so be aware.
+
+
+const parseF = s => isNaN(parseFloat(s)) ? null : parseFloat(s)
+
+
