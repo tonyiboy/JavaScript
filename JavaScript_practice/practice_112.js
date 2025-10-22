25238004bca2cee7ffd6e0bd6b8d73f1c7484e19
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-23 00:34:43 +0200

    practice_112

---
+++ JavaScript_practice/practice_112.js
@@ -0,0 +1,20 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+function bmi(weight, height) {
+    let result = weight / height ** 2
+  
+  return result <= 18.5 ? "Underweight" : result <= 25.0 ? "Normal" : result <= 30.0 ? "Overweight" : "Obese"
+
+}
+  
+
+
