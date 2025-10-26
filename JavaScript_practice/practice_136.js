26f5bc34f2519fc75c28bb8b3472cbf419d1ed88
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-26 22:33:50 +0100

    practice_136

---
+++ JavaScript_practice/practice_136.js
@@ -0,0 +1,27 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2){
+  switch(true) {
+      case operation === "+":
+        return value1 + value2;
+      break;
+      case operation === "-":
+        return value1 - value2;
+      break;
+      case operation === "*":
+        return value1 * value2;
+      break;
+      default: 
+        return value1 / value2;
+      break;
+  }
+}  
