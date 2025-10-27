9a66d6a5bd8aeb8933d9a8a3b5639e87774731f2
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-27 15:49:42 +0100

    practice_140

---
+++ JavaScript_practice/practice_140.js
@@ -0,0 +1,6 @@
+// Create a function that accepts a parameter representing a name and returns the message: 
+// "Hello, <name> how are you doing today?".
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
