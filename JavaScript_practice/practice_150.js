372039cfb9a59b1043af730718ff363623ae74b9
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-30 00:03:13 +0100

    practice_150

---
+++ JavaScript_practice/practice_150.js
@@ -0,0 +1,19 @@
+// Given a string, you have to return a string in which each character 
+// (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+function doubleChar(str) {
+  let result = ""
+  
+  for (let i = 0; i < str.length; i++) {
+    
+      result += str[i] + str[i]
+    
+  }
+  
+  return result
+}
