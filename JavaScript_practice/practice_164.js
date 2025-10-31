c0362d7311b7b434862a00b7fce122041d222aac
tonyiboy <antoniocodinggithub@gmail.com>
2025-10-31 18:05:53 +0100

    practice_164

---
+++ JavaScript_practice/practice_164.js
@@ -0,0 +1,44 @@
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let result = []
+
+  let human = 0
+  let catYears = 0
+  let dogYears = 0
+
+  for (let i = 0; i < humanYears; i++) {
+      if (i === 0) {
+        human = humanYears
+        catYears = 15
+        dogYears = 15
+      }else if (i === 1) {
+        catYears += 9
+        dogYears += 9
+      }else {
+        catYears += 4
+        dogYears += 5
+      } 
+  }
+
+  result.push(human, catYears, dogYears)
+
+  return result
+}
