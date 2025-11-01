52c182d63f8cf31e0961e2200700cffed897d7d8
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-01 21:08:59 +0100

    practice_174

---
+++ JavaScript_practice/practice_174.js
@@ -0,0 +1,10 @@
+// Write a function that accepts a non-negative integer n and a string s as parameters, 
+// and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
