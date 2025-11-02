6e79986a54d3f784666fd1b63908e14282b50aaa
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-02 20:13:00 +0100

    practice_185

---
+++ JavaScript_practice/practice_185.js
@@ -0,0 +1,10 @@
+// Create a combat function that takes the player's current health and the amount of damage received, 
+// and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+  let result = health - damage
+  
+  if (result > 0) {
+    return result
+  }else return 0
+}
