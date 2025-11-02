5f5bb26ccf7f18543d2b6dd0ccb1e6a862fd866d
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-02 20:12:54 +0100

    practice_184

---
+++ JavaScript_practice/practice_184.js
@@ -0,0 +1,8 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. 
+// If the value entered is a string it should return "Error".
+
+function problem(x){
+  if (typeof x === "number") {
+    return x * 50 + 6
+  }else return "Error"
+}
