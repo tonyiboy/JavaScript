9d92c9a7854b335d865aede0c575fed9d910aa30
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-02 20:12:27 +0100

    practice_181

---
+++ JavaScript_practice/practice_181.js
@@ -0,0 +1,37 @@
+// Character recognition software is widely used to digitise printed texts. 
+// Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), 
+// are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. 
+// You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+function correct(string) {
+	let result = ""
+  
+  for (let i = 0; i < string.length; i++) {
+    
+     switch (true) {
+      case string[i] === "5":
+        result += "S";
+      break;
+      case string[i] === "0":
+         result += "O";
+      break;
+      case string[i] === "1":
+         result += "I";
+      break;
+      default:
+         result += string[i];
+      break;
+  }
+  }
+  
+  return result
+}
