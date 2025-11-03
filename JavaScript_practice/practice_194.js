2f83026b841e8fcc7ed4813ccbf4028a595329f5
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-03 15:58:22 +0100

    practice_194

---
+++ JavaScript_practice/practice_194.js
@@ -0,0 +1,12 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+function getChar(c){
+  return String.fromCodePoint(c)
+}
