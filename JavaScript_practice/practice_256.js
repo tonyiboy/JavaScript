b54dbb3f40d0a4f1936aba1992e3480381e52c9b
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-10 21:53:35 +0100

    practice_256

---
+++ JavaScript_practice/practice_256.js
@@ -0,0 +1,20 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as 
+// if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x){
+
+  let result = 0
+
+  for (let i = 0; i < x.length; i++) {
+
+    result += Number(x[i])
+
+  }
+
+  return result
+
+}
+
+
