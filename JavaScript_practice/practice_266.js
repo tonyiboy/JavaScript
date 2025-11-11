6aa8ec7815a803512fe72f2b027f509933f5150f
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-11 11:47:04 +0100

    practice_266

---
+++ JavaScript_practice/practice_266.js
@@ -0,0 +1,16 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+  if (card.at(-1) === "♣") return "clubs"
+  if (card.at(-1) === "♦") return "diamonds"
+  if (card.at(-1) === "♥") return "hearts"
+  if (card.at(-1) === "♠") return "spades"
+
+}
