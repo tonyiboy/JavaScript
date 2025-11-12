de1689b88db36690ef0fc3c0615cbd3b206e0de1
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-12 23:47:16 +0100

    practice_277

---
+++ JavaScript_practice/practice_277.js
@@ -0,0 +1,5 @@
+//Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+
+let threeLastCh = "father"
+
+alert(threeLastCh.slice(-3)) 
