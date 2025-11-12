5bf17019ff16efdc8158d442dd2584d89190aae7
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-12 23:47:29 +0100

    practice_278

---
+++ JavaScript_practice/practice_278.js
@@ -0,0 +1,9 @@
+// *Functions*
+// Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
+
+function minusFiveAndAbs(num1,num2,num3,num4,num5){
+    let difference = 100 - num1 - num2 - num3 - num4 -num5
+    alert(Math.abs(difference))
+  }
+  
+  minusFiveAndAbs(27,83,42,1,5) 
