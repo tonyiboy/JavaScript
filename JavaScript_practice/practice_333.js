a4396fa110de8f215ad2fb6e653c1ce144bc7c4b
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-30 00:19:41 +0100

    practice_333

---
+++ JavaScript_practice/practice_333.js
@@ -0,0 +1,14 @@
+// Given a set of numbers, return the additive inverse of each. 
+// Each positive becomes negatives, and the negatives become positives.
+
+// [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+// [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+// [] --> []
+// You can assume that all values are integers. Do not mutate the input array.
+
+const invert = array => array.map(item => -item)
+   
+
+
+console.log(invert([1,2,3,4,5]));
+
