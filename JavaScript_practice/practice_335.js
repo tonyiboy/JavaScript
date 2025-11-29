abbd30eb962f0baaa0e8c0408f81cec08106464c
tonyiboy <antoniocodinggithub@gmail.com>
2025-11-30 00:19:53 +0100

    practice_335

---
+++ JavaScript_practice/practice_335.js
@@ -0,0 +1,17 @@
+// You will be given an array a and a value x. 
+// All you need to do is check whether the provided array contains the value.
+
+// a can contain numbers or strings. x can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  
+   let result = a.includes(x)
+  
+  return result || false
+  
+  
+}
+
+console.log(check([101, 45, 75, 105, 99, 107], 107));
