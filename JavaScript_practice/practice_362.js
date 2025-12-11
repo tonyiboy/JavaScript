7b698e5d648fedbaf94bf8be0f2e144dacf539a4
tonyiboy <antoniocodinggithub@gmail.com>
2025-12-11 15:03:46 +0100

    practice_362

---
+++ JavaScript_practice/practice_362.js
@@ -0,0 +1,23 @@
+// In this simple exercise, you will write a function that takes two integers; n and limit; 
+// and returns a list of the multiples of n up to and possibly including limit.
+
+// It is guaranteed that n > 0 and limit >= n.
+
+// For example, if the parameters passed are (2, 6), the function 
+// should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+// Examples
+// n = 2; limit = 6 --> [2, 4, 6]
+// n = 2; limit = 5 --> [2, 4]
+
+function findMultiples(integer, limit) {
+  let result = []
+  let i = integer
+
+  do {
+    result.push(i)
+    i += integer
+  } while ( i <= limit)
+
+  return result
+}
