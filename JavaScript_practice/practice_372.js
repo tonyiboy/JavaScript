a68299ea1bf9b43cebdc62213a780563ac540bfd
tonyiboy <antoniocodinggithub@gmail.com>
2025-12-12 23:30:36 +0100

    practice_372

---
+++ JavaScript_practice/practice_372.js
@@ -0,0 +1,36 @@
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// "Hi!" --> "H!!"
+// "!Hi! Hi!" --> "!H!! H!!"
+// "aeiou" --> "!!!!!"
+// "ABCDE" --> "!BCD!"
+
+function replace(s) {
+  
+    
+    
+    let result = ""
+    let i = 0
+
+    do{
+        if(s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u" ||
+            s[i] === "A" || s[i] === "E" || s[i] === "I" || s[i] === "O" || s[i] === "U") {
+           result += "!" 
+        } else result += s[i]
+        i++
+    }while ((i < s.length))
+
+
+
+    if (s === "") return ""
+    else return result
+    
+}
+
+
+
+
+
+
