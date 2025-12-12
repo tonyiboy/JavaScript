0a22a11af3200bb4611de776f64250665b15d80c
tonyiboy <antoniocodinggithub@gmail.com>
2025-12-12 23:29:59 +0100

    practice_367

---
+++ JavaScript_practice/practice_367.js
@@ -0,0 +1,20 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+function reverseList(list) {
+  let result = []
+  
+  for (let i of list) {
+    result = [result.push(i)] + result
+   
+    
+  }
+  
+  return result
+}
+
+console.log(reverseList([1,2,3,4]));
+
