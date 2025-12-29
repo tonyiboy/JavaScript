2e744827b11bd033a9f44e1e1886250f26ee41ed
tonyiboy <antoniocodinggithub@gmail.com>
2025-12-29 21:55:31 +0100

    practice_416

---
+++ JavaScript_practice/practice_416.js
@@ -0,0 +1,32 @@
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+// Note: you shouldn't escape the \n, it should work as a new line.
+
+function toCsvText(array) {
+   let result = ""
+
+    for (let i = 0; i < array.length; i++) {
+      if (i !== array.length -1) {
+        result += `${array[i].join()}\n`
+      } else result += `${array[i].join()}`
+    }
+
+    return result
+}
