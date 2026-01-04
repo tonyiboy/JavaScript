233f19615e2ebe049d2f6ece17ae15b8336f20b0
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-04 23:21:28 +0100

    practice_448

---
+++ JavaScript_practice/practice_448.js
@@ -0,0 +1,19 @@
+// Prolog:
+// This kata series was created for friends of mine who just started to learn programming. 
+// Wish you all the best and keep your mind open and sharp!
+
+// Task:
+// Write a function that will accept two parameters: variable and type 
+// and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+// 42, "number"   --> true
+// "42", "number" --> false
+
+function typeValidation(variable, type) {
+  
+  if(typeof variable === type) {
+    return true 
+  } else return false 
+  
+}
