e08f5d9e1dd32adbd6e4988f747086e751f7d15b
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-08 23:54:19 +0100

    practice_460

---
+++ JavaScript_practice/practice_460.js
@@ -0,0 +1,17 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+function findSmallestInt(arr) {
+
+    return Math.min(...arr)
+
+   
+
+}
+
+console.log(findSmallestInt([78,56,232,12,8])); 
