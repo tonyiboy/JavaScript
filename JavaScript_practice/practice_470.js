dc57a99b6f3d80a237c68fa8126176c9bcf03cf0
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-10 09:51:09 +0100

    practice_470

---
+++ JavaScript_practice/practice_470.js
@@ -0,0 +1,46 @@
+// When provided with a number between 0-9, return it in words. 
+// Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+
+// Output: "One".
+
+function switchItUp(number){ 
+  switch(number) {
+      case 1:
+         return "One";
+        break;
+      case 2:
+        return "Two";
+        break;
+      case 3:
+        return "Three";
+        break;
+      case 4:
+        return "Four";
+        break;
+      case 5:
+        return "Five";
+        break;
+      case 6:
+        return "Six";
+        break;
+      case 7:
+        return "Seven";
+        break;
+      case 8:
+        return "Eight";
+        break;
+      case 9:
+        return "Nine";
+        break;
+      case 0:
+        return "Zero";
+        break;
+  }
+}
+
+
+
+
+
