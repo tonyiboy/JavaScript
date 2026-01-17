02427674da0e1743d79cb5e0c6bce3df31b7ee97
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-17 20:05:27 +0100

    practice_485

---
+++ JavaScript_practice/practice_485.js
@@ -0,0 +1,7 @@
+// Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+function convertToInteger(str) {
+    return parseInt(str,2)
+    }
+    
+    convertToInteger("10011"); 
