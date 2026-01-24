b3d108b7de13ed07a40b99af9c472082d9a72935
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-24 23:19:37 +0100

    practice_492

---
+++ JavaScript_practice/practice_492.js
@@ -0,0 +1,12 @@
+// Very simple, given a number (integer / decimal / both depending on the language), 
+// find its opposite (additive inverse). 
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return -number
+}
