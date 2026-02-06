4a9e97788faf174f540fddf55cf2ed8c5a570e4c
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-06 19:06:33 +0100

    practice_502

---
+++ JavaScript_practice/practice_502.js
@@ -0,0 +1,24 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level 
+
+// Note2: the input data can be: boolean array, array of objects, 
+// array of string arrays, array of number arrays... 😕
+
+function printArray(array){
+  let result = ""
+  let i = 0
+  
+  do {
+    result += array[i] + ","
+    i++
+  }while (i < array.length) 
+  
+  return result.slice(0,-1)
+}
