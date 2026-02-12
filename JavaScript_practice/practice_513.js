6f443bd52ab6e380c57e68bdabaa4ad84f676e36
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-12 23:45:31 +0100

    practice_513

---
+++ JavaScript_practice/practice_513.js
@@ -0,0 +1,24 @@
+// Timmy & Sarah think they are in love, but around where they live, 
+// they will only know once they pick a flower each. If one of the flowers has an even number 
+// of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true 
+// if they are in love and false if they aren't.
+
+
+
+function lovefunc(flower1, flower2){
+  switch (true) {
+    case flower1 % 2 === 0 && flower2 % 2 === 0:
+    case flower1 % 2 !== 0 && flower2 % 2 !== 0: 
+      return false;
+    break;
+    default:
+      return true 
+  }
+    
+}
+
+
+ 
+
