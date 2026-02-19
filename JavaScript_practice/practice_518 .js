bc56374a88d2228f6721aed55a60257814d51d7b
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-19 23:39:56 +0100

    practice_518

---
+++ JavaScript_practice/practice_518 .js
@@ -0,0 +1,21 @@
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) { 
+  
+  switch (0) { 
+    case n % 2:
+      return true;
+    break;
+    default:
+      return false;
+    break;
+  }
+}
