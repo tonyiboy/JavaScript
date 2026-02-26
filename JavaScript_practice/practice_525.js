d155b246c85f990141d50446ccb458e78db999f9
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-26 23:46:09 +0100

    practice_525

---
+++ JavaScript_practice/practice_525.js
@@ -0,0 +1,17 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. 
+// If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
+// If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+   let result = []
+
+  x.forEach(item => {
+   if (item === "good") result.push(item)  
+  })
+     
+
+    return result.length === 0 ? 'Fail!' : result.length <= 2 ? 'Publish!' : 'I smell a series!'
+
+}
